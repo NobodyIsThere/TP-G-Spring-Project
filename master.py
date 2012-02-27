f5ab9f90d796bec591fd215e5582790492b876cc
Adam <hookaduckling@hotmail.com>
2012-02-27 17:48:02 +0000

    Fixed it.

--- master.py
+++ master.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
+# For some reason it's now UTF-8
 # Skeleton code starting point for Yr 1 Theory Group Project
 
-from __future__ import print_function, division
 from visual import *
 from visual.controls import *
 from random import *
@@ -28,10 +29,8 @@ class Planet:
 #function to create planets with set mass and initial distance but random position and colour
 def create_planet(initial_distance,m):
     angle = randrange(0, 6, 1) # random angle between 0 and 2pi with steps of pi/8
-    
     planet_pos = initial_distance * vector(sin(angle),cos(angle),0) # initial position vector of Planet
     mag_v = (sqrt(G*M/initial_distance))#magnitude of velocity of planet
-<<<<<<< HEAD
     print(m*mag_v**2/initial_distance, G*M*m/initial_distance**2)
     planet_v = mag_v * -vector(cos(angle), sin(angle), 0) # initial velocity of planet
     planet = Planet(pos=planet_pos, velocity=planet_v, mass=m, lastpos=vector(0,0,0))
@@ -79,40 +78,22 @@ scene2 = display(title='Other frame', x=500, visible=True)
 scene1.select()
 
 step = 0 # Current step
-=======
-    #print(m*mag_v**2/initial_distance, G*M*m/initial_distance**2)
-    planet_v = mag_v * -vector(-cos(angle), sin(angle), 0) # initial velocity of planet
-    planet = sphere(pos=planet_pos,radius=0.05,color=color.blue, mass=m, velocity = planet_v)
-    planets.append(planet)
-
-def change_v(angle):
-    return
-    
-
-# Definitions
->>>>>>> 7f6ee396e83b1f4c640622b52322850a32f63476
 dt = 0.001 # timestep
-G = 1.0#0.0001
+G = 1.0
 
 # List of planets
 planets = []
 
 # Star
-<<<<<<< HEAD
 M = 1000# mass of star (G == 1)
 Star = Planet(pos=vector(0,0,0), velocity=vector(0,0,0), mass=M, lastpos=vector(0,0,0))
 Star.avatar1 = sphere(pos=vector(0,0,0),radius=0.1,color=color.yellow)
-=======
-M = (1000)# mass of star (G == 1)
-Star = sphere(pos=vector(0,0,0),radius=0.1,color=color.yellow, velocity=vector(0,0,0), mass=M)
->>>>>>> 7f6ee396e83b1f4c640622b52322850a32f63476
 planets.append(Star)
 
 # The index of the planet we're following
 following = 0
 
 # Planets
-<<<<<<< HEAD
 create_planet(3,1)
 create_planet(4,1)
 
@@ -131,27 +112,6 @@ while True:
     scene1.select()
 
     cwindow.interact()
-=======
-create_planet(1,1)
-create_planet(4,0.5)
-#create_planet(6,2)
-create_planet(0.5,0.2)
-create_planet(1.5,0.8)
-create_planet(2.5,1.5)
-
-# Function definitions
-
-# Returns force on obj1 due to obj2
-def calc_gravity(obj1, obj2):
-    magnitude = (G*obj1.mass*obj2.mass)/(((obj2.pos-obj1.pos).mag)**2)
-    ##magnitude = 0
-    direction = (obj2.pos - obj1.pos).norm()
-    return magnitude*direction
-
-while True:
-
-    rate(10)  # max fps
->>>>>>> 7f6ee396e83b1f4c640622b52322850a32f63476
 
     for planet in planets:
         for otherplanet in planets:
@@ -165,7 +125,6 @@ while True:
         # Update position
         if planet!=Star:
             planet.pos = planet.pos+(dt*planet.velocity)
-<<<<<<< HEAD
 
     if keplers_law == True:
         if step%keplers_law_period==0:
@@ -194,5 +153,3 @@ while True:
     Star.avatar1.color = color.yellow
     Star.avatar2.color = color.yellow
     step+=1
-=======
->>>>>>> 7f6ee396e83b1f4c640622b52322850a32f63476
